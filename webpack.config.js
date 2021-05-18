1a406613eb8953a0d5a001826a4208b49d4001b7
GuoFei <guofei0723@gmail.com>
2021-05-18 22:34:22 +0800

    添加gtag

--- webpack.config.js
+++ webpack.config.js
@@ -3,6 +3,16 @@ const HtmlWebpackPlugin = require('html-webpack-plugin')
 const { CleanWebpackPlugin } = require('clean-webpack-plugin')
 const CopyWebpackPlugin = require('copy-webpack-plugin')
 
+const gtag = `<script async src="https://www.googletagmanager.com/gtag/js?id=G-D9J2ZVJPLB"></script>
+<script>
+  window.dataLayer = window.dataLayer || [];
+  function gtag(){dataLayer.push(arguments);}
+  gtag('js', new Date());
+
+  gtag('config', 'G-D9J2ZVJPLB');
+</script>
+`
+
 module.exports = {
   entry: './snake.js',
   mode: 'development',
@@ -27,7 +37,9 @@ module.exports = {
   plugins: [
     new CleanWebpackPlugin(),
     new HtmlWebpackPlugin({
+      title: 'Snake',
       template: './index.html',
+      gtag: process.env.NODE_ENV === 'production' ? gtag : ''
     }),
     new CopyWebpackPlugin({
       patterns: [
